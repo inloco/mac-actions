883fa94d56b1391d8ba3b1458bf7793ca7743bce
Pedro Tôrres <pedro.torres@incognia.com>
2022-03-15 23:03:47 -0300

    feat(continuous): improve parallel xcode purge
    Signed-off-by: Pedro Tôrres <pedro.torres@incognia.com>

--- hack/purge-xcode.sh
+++ hack/purge-xcode.sh
@@ -7,11 +7,50 @@ XCODE_REAL=$(readlink "${XCODE_LINK}")
 unlink "${XCODE_LINK}"
 mv "${XCODE_REAL}" "${XCODE_LINK}"
 
+sudo xcode-select -s "${XCODE_LINK}"
+
+find /Applications -type l -maxdepth 1 -delete
+
 {
-    for D in /Applications/Xcode_*
+    for D in /Applications/Xcode_*.app/Contents/Developer/Platforms/*.platform/Library/Developer/CoreSimulator/Profiles/Runtimes/*.simruntime/Contents/Resources/RuntimeRoot
     do
-        echo rsync -aP --del /var/empty/ "${D}"
+        echo "find '${D}' -type f -delete"
     done
 } | parallel --lb ::::
 
-rm -fR /Applications/Xcode_*
+{
+    for D in /Applications/Xcode_*.app/Contents/Developer/Platforms/*.platform/Library/Developer/CoreSimulator/Profiles/Runtimes/*.simruntime
+    do
+        echo "find '${D}' -type f -delete"
+    done
+} | parallel --lb ::::
+
+{
+    for D in /Applications/Xcode_*.app/Contents/Developer/Platforms/*.platform
+    do
+        echo "find '${D}' -type f -delete"
+    done
+} | parallel --lb ::::
+
+{
+    for D in /Applications/Xcode_*.app/Contents/Developer
+    do
+        echo "find '${D}' -type f -delete"
+    done
+} | parallel --lb ::::
+
+{
+    for D in /Applications/Xcode_*.app/Contents
+    do
+        echo "find '${D}' -type f -delete"
+    done
+} | parallel --lb ::::
+
+{
+    for D in /Applications/Xcode_*.app
+    do
+        echo "find '${D}' -type f -delete"
+    done
+} | parallel --lb ::::
+
+# rm -fR /Applications/Xcode_*.app
